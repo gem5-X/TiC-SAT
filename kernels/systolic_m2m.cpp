c2c497b77ee53ea76ceb84af3fc5fa3ce3688429
Alireza Amirshahi <a.amirshahi37@gmail.com>
2022-01-13 22:44:40 +0100

    add systolic matrix multiplication to the kernels

---
+++ kernels/systolic_m2m.cpp
@@ -0,0 +1,46 @@
+//
+// Created by alireza on 1/13/22.
+//
+
+#include <cstdint>
+#include "systolic_m2m.h"
+
+SystolicMatrixMultiplication::SystolicMatrixMultiplication() = default;
+
+void SystolicMatrixMultiplication::loadWeights(int row, uint32_t val) {
+    for (int i=0; i < W_DIM; i++){
+        int idx= row * W_DIM + i;
+        uint8_t currVal = (val >> (8 * i)) & 0xff;
+        weights[idx] = currVal;
+    }
+}
+
+uint32_t SystolicMatrixMultiplication::streamInOut(uint32_t val) {
+    // Split the input to an array
+    for (int i=0; i < W_DIM; i++){
+        uint8_t currVal = (val >> (8 * i)) & 0xff;
+        inputMemory[i*W_DIM] = currVal; // First column of the input memory
+    }
+
+    // Multiply the input to the weight and accumulate to the output
+    for (int i=W_DIM*W_DIM-1; i>=0 ; i--){
+        outputMemory[i+W_DIM] = (inputMemory[i] * weights[i]) + outputMemory[i];
+    }
+
+    // Return the output
+    uint32_t result = 0;
+    for (int i = 0; i < W_DIM; i++) {
+        result |=  outputMemory[W_DIM*W_DIM + i]<< (8 * i);
+    }
+
+    // Shift the input memory to the right
+    for (int i = 0; i < W_DIM; i++) {
+        for (int j=W_DIM-1; j>0; j--){
+            inputMemory[i*W_DIM + j] = inputMemory[i*W_DIM + j-1];
+        }
+    }
+
+    return result;
+
+}
+
---
+++ kernels/systolic_m2m.h
@@ -0,0 +1,21 @@
+//
+// Created by alireza on 1/13/22.
+//
+
+#ifndef FVLLMONTIMATRIXMULTIPLICATION_SYSTOLIC_M2M_H
+#define FVLLMONTIMATRIXMULTIPLICATION_SYSTOLIC_M2M_H
+
+#define W_DIM 4
+
+class SystolicMatrixMultiplication {
+private:
+    uint8_t weights[W_DIM * W_DIM]{};
+    uint8_t outputMemory[W_DIM * (W_DIM+1)]{};
+    uint8_t inputMemory[W_DIM * W_DIM]{};
+public:
+    SystolicMatrixMultiplication();
+    void loadWeights(int row, uint32_t  val);
+    uint32_t streamInOut(uint32_t val);
+};
+
+#endif //FVLLMONTIMATRIXMULTIPLICATION_SYSTOLIC_M2M_H
