76a8c59fb8ed4dbd851f8e85e6f518a977c0366a
Alireza Amirshahi <a.amirshahi37@gmail.com>
2022-03-02 14:34:34 +0100

    transpose matrix k

--- kernels/batchgemm.h
+++ kernels/batchgemm.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include "util.h"
-
-namespace lh{
-    template<class T>
-    void attn_qk(std::size_t batch_size, std::size_t num_heads, std::size_t seq_len, std::size_t hidden_size, T* query, T* key, T* output, const T** q_array, const T** k_array, T** pointer_qk_array);
-
-    template<class T>
-    void attn_sv(std::size_t batch_size, std::size_t num_heads, std::size_t seq_len, std::size_t hidden_size, T* sim, T* value, T* output, const T** sim_array, const T** value_array, T** pointer_sv_array);
-
-    template<class T>
-            void batchMatMul(std::size_t batch_size, std::size_t seq_len, float* input, float* output, float *weight,
-                             std::size_t input_size_, std::size_t output_size_);
-}
+//void attn_qk(std::size_t batch_size, std::size_t num_heads, std::size_t seq_len, std::size_t hidden_size, T* query, T* key, T* output, const T** q_array, const T** k_array, T** pointer_qk_array);
+//
+//void attn_sv(std::size_t batch_size, std::size_t num_heads, std::size_t seq_len, std::size_t hidden_size, T* sim, T* value, T* output, const T** sim_array, const T** value_array, T** pointer_sv_array);
+//
+//void batchMatMul(std::size_t batch_size, std::size_t seq_len, float* input, float* output, float *weight,
+//                 std::size_t input_size_, std::size_t output_size_);
+class Transpose {
+public:
+    static void transpose(const uint32_t* input, uint32_t* output, std::size_t width, std::size_t height) ;
+};
