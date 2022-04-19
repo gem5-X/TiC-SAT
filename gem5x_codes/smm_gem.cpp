8207831df2338685c46191788cf25fd802a9e3a6
Alireza Amirshahi <a.amirshahi37@gmail.com>
2022-04-19 15:21:11 +0200

    add hierarchical tiling for L2 utilization

--- gem5x_codes/smm_gem.cpp
+++ gem5x_codes/smm_gem.cpp
@@ -9,6 +9,7 @@
 #define W_DATA 4
 #define MAX_COL 4
 #define KERNEL_DIM 16
+#define L2_DIM 32
 #define mem2d(data, data_len, row, col)   data[((row)*(data_len))+(col)]
 
 void add8in32(uint32_t &memory, uint32_t &systolicResult);
@@ -106,53 +107,60 @@ uint64_t smmParamWrite(uint64_t rm, uint64_t rn, uint64_t ra) {
 void smmCompute(std::size_t seq_len, const uint32_t *input, uint32_t *output, uint32_t *weights,
                 std::size_t input_size_, std::size_t output_size_) {
 
-    for (int tileRow = 0; tileRow < input_size_ / KERNEL_DIM; tileRow++) {
-        for (int tileCol = 0; tileCol < output_size_ / KERNEL_DIM; tileCol++) {
-            // Load the kernel with the corresponding weight
-            int rowStart = tileRow * KERNEL_DIM;
-            int colStart = tileCol * KERNEL_DIM / W_DATA;
-            int rowBlockSize = KERNEL_DIM;
-            int colBlockSize = KERNEL_DIM / W_DATA;
-            uint32_t* wPtr = weights + rowStart * (output_size_/W_DATA);
-            for (int i = rowStart; i < rowStart + rowBlockSize; i++) {
-                for (int j = colStart; j < colStart + colBlockSize; j++) {
-                    uint32_t weight = * (wPtr + j);
-                    smmParamWrite(i - rowStart, j - colStart, weight);
-                }
-                wPtr += output_size_ / W_DATA;
-            }
-
-            // Process the multiplication
-            int base_col_idx = tileRow * MAX_COL;
-            int outputIndex = 0;
-            uint32_t mult;
-            const uint32_t * inPtr = input + base_col_idx;
-            for (int i = 0; i < seq_len; i++) {
-                for (int j = 0; j < MAX_COL; j++) {
-                    if (j == MAX_COL - 1) {
-                        mult = smmStream(*(inPtr + j));
-                    } else {
-                        mult = smmQueue(j % MAX_COL, *(inPtr + j));
+    int rowMaxL2 = std::min(L2_DIM, (int) (input_size_ / KERNEL_DIM));
+    int colMaxL2 = std::min(L2_DIM, (int) (output_size_ / KERNEL_DIM));
+    std::cout<< rowMaxL2 << "\t\t" << colMaxL2 <<std::endl;
+    for (int l2Row=0; l2Row < (input_size_ / KERNEL_DIM) / rowMaxL2; l2Row++){
+        for (int l2Col=0; l2Col < (output_size_ / KERNEL_DIM) / colMaxL2; l2Col++){
+            for (int tileRow = 0; tileRow < rowMaxL2; tileRow++) {
+                for (int tileCol = 0; tileCol < colMaxL2; tileCol++) {
+                    // Load the kernel with the corresponding weight
+                    int rowStart = (l2Row * rowMaxL2 + tileRow) * KERNEL_DIM;
+                    int colStart = (l2Col * colMaxL2 + tileCol) * KERNEL_DIM / W_DATA;
+                    int rowBlockSize = KERNEL_DIM;
+                    int colBlockSize = KERNEL_DIM / W_DATA;
+                    uint32_t* wPtr = weights + rowStart * (output_size_/W_DATA);
+                    for (int i = rowStart; i < rowStart + rowBlockSize; i++) {
+                        for (int j = colStart; j < colStart + colBlockSize; j++) {
+                            uint32_t weight = * (wPtr + j);
+                            smmParamWrite(i - rowStart, j - colStart, weight);
+                        }
+                        wPtr += output_size_ / W_DATA;
                     }
 
-                    if ((i * MAX_COL + j) >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) {    // check if the output is valid
-                        add8in32(mem2d(output, output_size_ / W_DATA, outputIndex / colBlockSize,
-                                       colStart + outputIndex % colBlockSize), mult);
-                        outputIndex++;
+                    // Process the multiplication
+                    int base_col_idx = (l2Row * rowMaxL2 + tileRow) * MAX_COL;
+                    int outputIndex = 0;
+                    uint32_t mult;
+                    const uint32_t * inPtr = input + base_col_idx;
+                    for (int i = 0; i < seq_len; i++) {
+                        for (int j = 0; j < MAX_COL; j++) {
+                            if (j == MAX_COL - 1) {
+                                mult = smmStream(*(inPtr + j));
+                            } else {
+                                mult = smmQueue(j % MAX_COL, *(inPtr + j));
+                            }
+
+                            if ((i * MAX_COL + j) >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) {    // check if the output is valid
+                                add8in32(mem2d(output, output_size_ / W_DATA, outputIndex / colBlockSize,
+                                               colStart + outputIndex % colBlockSize), mult);
+                                outputIndex++;
+                            }
+                        }
+                        inPtr += (input_size_ / W_DATA);
                     }
-                }
-                inPtr += (input_size_ / W_DATA);
-            }
-            for (int i = seq_len * MAX_COL; i < MAX_COL * (seq_len + 2 * KERNEL_DIM - 1) - 1; i++) {
-                if ((i % MAX_COL) == MAX_COL - 1) {
-                    mult = smmStream(0);
-                } else {
-                    mult = smmQueue(i % MAX_COL, 0);
-                }
-                if (i >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) { // check if the output is valid
-                    add8in32(mem2d(output, output_size_ / W_DATA, outputIndex / colBlockSize,
-                                   colStart + outputIndex % colBlockSize), mult);
-                    outputIndex++;
+                    for (int i = seq_len * MAX_COL; i < MAX_COL * (seq_len + 2 * KERNEL_DIM - 1) - 1; i++) {
+                        if ((i % MAX_COL) == MAX_COL - 1) {
+                            mult = smmStream(0);
+                        } else {
+                            mult = smmQueue(i % MAX_COL, 0);
+                        }
+                        if (i >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) { // check if the output is valid
+                            add8in32(mem2d(output, output_size_ / W_DATA, outputIndex / colBlockSize,
+                                           colStart + outputIndex % colBlockSize), mult);
+                            outputIndex++;
+                        }
+                    }
                 }
             }
         }
