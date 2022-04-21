4ecea25df493f0c09a99ee1fe735ad917874c533
Alireza Amirshahi <a.amirshahi37@gmail.com>
2022-04-22 00:05:41 +0200

    add hierarchical loop to support L1 and L2

--- kernels/matMulSystolic.cc
+++ kernels/matMulSystolic.cc
@@ -8,63 +8,71 @@ void MatMulSystolic::compute(std::size_t seq_len, const uint32_t *input, uint32_
                              std::size_t input_size_, std::size_t output_size_) {
 
     SystolicMatrixMultiplication systolicMM;
-    int rowMaxL2 = std::min(2, (int) (input_size_ / KERNEL_DIM));
-    int colMaxL2 = std::min(2, (int) (output_size_ / KERNEL_DIM));
+    int rowMaxL1 = 4;
+    int colMaxL1 = 4;
+    int rowMaxL2 = std::min(16, (int) (input_size_ / KERNEL_DIM / rowMaxL1));
+    int colMaxL2 = std::min(32, (int) (output_size_ / KERNEL_DIM / colMaxL1));
     std::cout<< rowMaxL2 << "\t\t" << colMaxL2 <<std::endl;
-    for (int l2Row=0; l2Row < (input_size_ / KERNEL_DIM) / rowMaxL2; l2Row++){
-        for (int l2Col=0; l2Col < (output_size_ / KERNEL_DIM) / colMaxL2; l2Col++){
+    for (int l2Row=0; l2Row < (input_size_ / KERNEL_DIM) / rowMaxL2 / rowMaxL1; l2Row++){
+        for (int l2Col=0; l2Col < (output_size_ / KERNEL_DIM) / colMaxL2 / colMaxL1; l2Col++){
             for (int seqBlockIdx = 0 ; seqBlockIdx < 4; seqBlockIdx++){
-                for (int tileRow = 0; tileRow < rowMaxL2; tileRow++) {
-                    for (int tileCol = 0; tileCol < colMaxL2; tileCol++) {
-                        //        std::cout << "Tile Column : " << tileCol << std::endl;
+                for (int tileRowL2 = 0; tileRowL2 < rowMaxL2; tileRowL2++) {
+                    for (int tileColL2 = 0; tileColL2 < colMaxL2; tileColL2++) {
+                        for (int tileRowL1 = 0; tileRowL1 < rowMaxL1; tileRowL1++) {
+                            for (int tileColL1 = 0; tileColL1 < colMaxL1; tileColL1++) {
+                                int tileRow = tileRowL2 * rowMaxL1 + tileRowL1;
+                                int tileCol = tileColL2 * colMaxL1 + tileColL1;
+                                //        std::cout << "Tile Column : " << tileCol << std::endl;
 
-                        // Load the kernel with the corresponding weight
-                        int rowStart = (l2Row * rowMaxL2 + tileRow) * KERNEL_DIM;
-                        int colStart = (l2Col * colMaxL2 + tileCol) * KERNEL_DIM / W_DATA;
-                        int rowBlockSize = KERNEL_DIM;
-                        int colBlockSize = KERNEL_DIM / W_DATA;
-                        uint32_t* wPtr = weights + rowStart * (output_size_/W_DATA);
-                        for (int i = rowStart; i < rowStart + rowBlockSize; i++) {
-                            for (int j = colStart; j < colStart + colBlockSize; j++) {
-                                uint32_t weight = * (wPtr + j);
-                                systolicMM.loadWeights(i - rowStart, j - colStart, weight);
-                            }
-                            wPtr += output_size_ / W_DATA;
-                        }
-
-                        // Process the multiplication
-                        int base_col_idx = (l2Row * rowMaxL2 + tileRow) * MAX_COL;
-                        int seqBlockLen = (int)(seq_len/4);
-                        int outputIndex =  0;
-                        uint32_t * outPtr = output + seqBlockIdx  * seqBlockLen * (output_size_/ W_DATA);
-                        uint32_t mult;
-                        const uint32_t * inPtr = input + base_col_idx + seqBlockIdx* seqBlockLen *(input_size_ / W_DATA);
-                        for (int i = 0; i < seqBlockLen; i++) {
-                            for (int j = 0; j < MAX_COL; j++) {
-                                if (j == MAX_COL - 1) {
-                                    mult = systolicMM.streamInOut(*(inPtr + j));
-                                } else {
-                                    mult = systolicMM.inputQueue(j % MAX_COL,*(inPtr + j));
+                                // Load the kernel with the corresponding weight
+                                int rowStart = (l2Row * rowMaxL2 * rowMaxL1 + tileRow) * KERNEL_DIM;
+                                int colStart = (l2Col * colMaxL2 * colMaxL1 + tileCol) * KERNEL_DIM / W_DATA;
+                                int rowBlockSize = KERNEL_DIM;
+                                int colBlockSize = KERNEL_DIM / W_DATA;
+                                uint32_t* wPtr = weights + rowStart * (output_size_/W_DATA);
+                                for (int i = rowStart; i < rowStart + rowBlockSize; i++) {
+                                    for (int j = colStart; j < colStart + colBlockSize; j++) {
+                                        uint32_t weight = * (wPtr + j);
+                                        systolicMM.loadWeights(i - rowStart, j - colStart, weight);
+                                    }
+                                    wPtr += output_size_ / W_DATA;
                                 }
 
-                                if ((i * MAX_COL + j) >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) {    // check if the output is valid
-                                    add8in32(mem2d(outPtr, output_size_ / W_DATA, outputIndex / colBlockSize,
-                                                   colStart + outputIndex % colBlockSize), mult);
-                                    outputIndex++;
+                                // Process the multiplication
+                                int base_col_idx = (l2Row * rowMaxL2* rowMaxL1 + tileRow) * MAX_COL;
+                                int seqBlockLen = (int)(seq_len/4);
+                                int outputIndex =  0;
+                                uint32_t * outPtr = output + seqBlockIdx  * seqBlockLen * (output_size_/ W_DATA);
+                                uint32_t mult;
+                                const uint32_t * inPtr = input + base_col_idx + seqBlockIdx* seqBlockLen *(input_size_ / W_DATA);
+                                for (int i = 0; i < seqBlockLen; i++) {
+                                    for (int j = 0; j < MAX_COL; j++) {
+                                        if (j == MAX_COL - 1) {
+                                            mult = systolicMM.streamInOut(*(inPtr + j));
+                                        } else {
+                                            mult = systolicMM.inputQueue(j % MAX_COL,*(inPtr + j));
+                                        }
+
+                                        if ((i * MAX_COL + j) >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) {    // check if the output is valid
+                                            add8in32(mem2d(outPtr, output_size_ / W_DATA, outputIndex / colBlockSize,
+                                                           colStart + outputIndex % colBlockSize), mult);
+                                            outputIndex++;
+                                        }
+                                    }
+                                    inPtr += (input_size_ / W_DATA);
                                 }
-                            }
-                            inPtr += (input_size_ / W_DATA);
-                        }
-                        for (int i = seqBlockLen * MAX_COL; i < MAX_COL * (seqBlockLen + 2 * KERNEL_DIM - 1) - 1; i++) {
-                            if ((i % MAX_COL) == MAX_COL - 1) {
-                                mult = systolicMM.streamInOut(0);
-                            } else {
-                                mult = systolicMM.inputQueue(i % MAX_COL, 0);
-                            }
-                            if (i >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) { // check if the output is valid
-                                add8in32(mem2d(outPtr, output_size_ / W_DATA, outputIndex / colBlockSize,
-                                               colStart + outputIndex % colBlockSize), mult);
-                                outputIndex++;
+                                for (int i = seqBlockLen * MAX_COL; i < MAX_COL * (seqBlockLen + 2 * KERNEL_DIM - 1) - 1; i++) {
+                                    if ((i % MAX_COL) == MAX_COL - 1) {
+                                        mult = systolicMM.streamInOut(0);
+                                    } else {
+                                        mult = systolicMM.inputQueue(i % MAX_COL, 0);
+                                    }
+                                    if (i >= (MAX_COL * (2 * KERNEL_DIM - 1) - 1)) { // check if the output is valid
+                                        add8in32(mem2d(outPtr, output_size_ / W_DATA, outputIndex / colBlockSize,
+                                                       colStart + outputIndex % colBlockSize), mult);
+                                        outputIndex++;
+                                    }
+                                }
                             }
                         }
                     }
