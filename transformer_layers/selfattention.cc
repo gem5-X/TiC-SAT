86f45c2251fd2e3b2e6e84dc5d1d3dc4f38a4c03
Alireza Amirshahi <a.amirshahi37@gmail.com>
2023-03-06 15:49:17 +0100

    pass the kernel size and max col to the functions

--- transformer_layers/selfattention.cc
+++ transformer_layers/selfattention.cc
@@ -5,10 +5,12 @@
 //#include <cstdint>
 
 SingleHeadSelfAttn::SingleHeadSelfAttn(std::size_t pre_seq_len, std::size_t input_dim, std::size_t head_hidden_size,
-                                       uint32_t ** weightVector) {
+                                       uint32_t ** weightVector, std::size_t kernel_dim, std::size_t max_col) {
 
     pre_seq_len_ = pre_seq_len;
     head_hidden_size_ = head_hidden_size;
+    kernel_size_ = kernel_dim;
+    max_col_ = max_col;
 
     query_layer = new Dense(input_dim, head_hidden_size, weightVector[0]);
     key_layer = new Dense(input_dim, head_hidden_size, weightVector[1]);
@@ -46,7 +48,8 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
     system("m5 dumpresetstats");
 //    std::cout<< "Before: " << std::endl;
 //    print_weight(key_layer_out, pre_seq_len_ * head_hidden_size_/64, 8* 2);
-    Transpose::transpose_rearranged(key_layer_out, key_transposed_layer_out, head_hidden_size_, pre_seq_len_, 8, 2);
+    Transpose::transpose_rearranged(key_layer_out, key_transposed_layer_out, head_hidden_size_, pre_seq_len_,
+                                    kernel_size_, max_col_);
 //    std::cout<< "After: " << std::endl;
 //    print_weight(key_transposed_layer_out, head_hidden_size_, pre_seq_len_ >> 2);
 //    getchar();
@@ -54,7 +57,7 @@ void SingleHeadSelfAttn::compute(std::size_t seq_len, uint32_t *input, uint32_t 
     smmCompute(seq_len, query_layer_out, attention_scores, key_transposed_layer_out, head_hidden_size_,
                             seq_len, false);
     system("m5 dumpresetstats");
-    softmax->computeRearranged(attention_scores, seq_len, 8);
+    softmax->computeRearranged(attention_scores, seq_len, kernel_size_);
     system("m5 dumpresetstats");
     smmCompute(seq_len, attention_scores, output, value_layer_out, seq_len, head_hidden_size_, false);
     system("m5 dumpresetstats");
--- transformer_layers/transformerBlock.cc
+++ transformer_layers/transformerBlock.cc
@@ -22,7 +22,8 @@ TransformerBlock::TransformerBlock(std::size_t pre_seq_len, std::size_t input_di
     ff_size_ = ff_size;
 
     for (int n =0; n< num_heads; n++){
-        selfatten[n] = new SingleHeadSelfAttn(pre_seq_len, input_dim, head_hidden_size, weightVector+n*3);
+        selfatten[n] = new SingleHeadSelfAttn(pre_seq_len, input_dim, head_hidden_size, weightVector+n*3,
+                                              kernelDim, maxCol);
     }
 
     condense = new Dense(num_heads* head_hidden_size, input_dim, weightVector[num_heads * 3]);
